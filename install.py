574876a935b4598eb54ca791bbebd7a5d1f2ed4f
Denis Angulo <denis.angulo@linekode.com>
2017-08-04 17:36:07 -0400

    install.py: initial changes, sorting out arguments

--- install.py
+++ install.py
@@ -7,19 +7,13 @@ parser.add_argument(
     help="""Name for the DRF project. Positional and must be first. Required.""",
     action='store',
 )
-parser.add_argument(
-    '--api-url-prefix',
-    help="""Prefix for the API endpoints.""",
-    action='store',
-)
-parser.add_argument(
-    '--proxy',
-    help="""Proxy information for pip to consume.""",
-    action='store',
-)
-parser.add_argument(
-    '--auth-type',
-    help="""Default authentication class(es) for DEFAULT_AUTHENTICATION_CLASSES in settings.py. 
-Options: any combination of 'basic', 'token', 'session'. Will be added in the order provided"""
-
-)
+parser.add_argument('--api-url-prefix', help="""
+Prefix for the API endpoints.
+""", action='store')
+parser.add_argument('--proxy', help="""
+Proxy information for pip to consume.
+""", action='store')
+parser.add_argument('--auth-type', help="""
+Default authentication class(es) for DEFAULT_AUTHENTICATION_CLASSES in settings.py. 
+Options: any combination of 'basic', 'token', 'session'. Will be added in the order provided
+""", action='store', choices=['basic', 'token', 'session'])
