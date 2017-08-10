0dac7ce31503a13fd4c33264ffefe84db63645e4
Denis Angulo <denis.angulo@linekode.com>
2017-08-10 14:11:48 -0400

    Added docstrings, and all optional methods to setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,33 @@ from subprocess import Popen, PIPE, run
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class SetupDrfSeed:
-
+    """Sets up the drf_seed project, along with some settings in
+    urls.py and settings.py"""
     def __init__(self):
         self._sanity_checks()
         self._parse_arguments()
+        self._import_settings(self._args.project_name)
+    
+    def _import_settings(self, proj):
+        """Utility method to provide a project/settings.py handle and a
+        settings.REST_FRAMEWORK handle"""
+        self.project_name = proj
+        self.settings_str = f'{self.project_name}/settings.py'
+        try:
+            self.settings = __import__(
+                f'{self.project_name}.settings',
+                globals(),
+                locals(),
+                ['*']
+            )
+            self.drf_settings = self.settings.REST_FRAMEWORK
+        except ModuleNotFoundError:
+            sys.exit(f'Could not locate {self.project_name}/settings.py, was '
+                    'this script run from the root dir?')
 
     def _sanity_checks(self):
+        """Checks that requirements are installed and determines the
+        python cli command to invoke"""
         try:
             outs, errs = Popen('sed --version', stdout=PIPE).communicate(timeout=5)
         except FileNotFoundError:
@@ -43,6 +64,7 @@ class SetupDrfSeed:
                     sys.exit('ERROR: python is not installed or is not found in $PATH')
 
     def _parse_arguments(self):
+        """Argument parsing"""
         parser = argparse.ArgumentParser(description="Setup basic drf_seed project")
         parser.add_argument('project_name', action='store', type=str, nargs='?',
             help='Name for the DRF project')
@@ -69,8 +91,10 @@ class SetupDrfSeed:
             help='Integer to set page size, ignored if --pagination is not set. If '
             '--pagination is provided, will default to 20 if not set.')
         parser.add_argument('--throttling', action='store', type=str, nargs='+',
-            help='Sets DEFAULT_THROTTLE_CLASSES in settings.py')
-        parser.add_argument('--static', action='store', type=str, nargs='?',
+            help='Sets DEFAULTTHROTTLE_CLASSES in settings.py')
+        parser.add_argument('--static-root', action='store', type=str, nargs='?',
+            const='static')
+        parser.add_argument('--static-url', action='store', type=str, nargs='?',
             const='static')
         parser.add_argument('--media-root', action='store', type=str, nargs='?',
             const='media')
@@ -79,6 +103,8 @@ class SetupDrfSeed:
         self._args = parser.parse_args()
 
     def _create_virtualenv(self):
+        """Creates virtualenv under the ~/project/virtualenv/ 
+        directory by calling python -m venv virtualenv"""
         if 'virtualenv' not in os.listdir(BASE_DIR):
             if 'win' in sys.platform:
                 command = f'{self.python_caller} -m venv {BASE_DIR}\\virtualenv'
@@ -88,6 +114,7 @@ class SetupDrfSeed:
                 run(command, stdout=PIPE)
 
     def _install_dependencies(self, proxy=None):
+        """Installs 'requirements.txt using pip"""
         if 'win' in sys.platform:
             command = f'{BASE_DIR}\\virtualenv\\Scripts\\pip '
             command += f"--proxy {proxy  + ' ' if proxy else ''}"
@@ -99,18 +126,19 @@ class SetupDrfSeed:
             command += f'install -r {BASE_DIR}/requirements.txt'
             run(command)
 
-    def _rename_drf_seed_instances(self, project_name):
+    def _rename_drf_seed_instances(self):
+        """Renames project's generic name with your own"""
         run(f'cd {BASE_DIR}')
         if 'drf_seed' in os.listdir(BASE_DIR):
             if 'win' in sys.platform:
-                run(f'sed -i s:drf_seed:{project_name}:g drf_seed/*')
-                run(f'move drf_seed {project_name}')
+                run(f'sed -i s:drf_seed:{self.project_name}:g drf_seed/*')
+                run(f'move drf_seed {self.project_name}')
             else:
-                run(f'sed -i s:drf_seed:{project_name}:g drf_seed/* && mv drf_seed')
-                run(f'mv drf_seed {project_name}')
+                run(f'sed -i s:drf_seed:{self.project_name}:g drf_seed/* && mv drf_seed')
+                run(f'mv drf_seed {self.project_name}')
 
-
-    def _set_router(self, project_name, router=None):
+    def _set_router(self, router=None):
+        """Sets default router to use, if any"""
         _ROUTERS = {
             'simple': 'SimpleRouter',
             'default': 'DefaultRouter',
@@ -118,12 +146,13 @@ class SetupDrfSeed:
         }
         if router:
             if router == 'none':
-                run(f'''sed -i "/router/d" {project_name}/urls.py''')
+                run(f'''sed -i "/router/d" {self.project_name}/urls.py''')
             else:
-                run(f'''sed -i "s:DefaultRouter:{_ROUTERS[router]}:g {project_name}/urls.py''')
+                run(f'''sed -i "s:DefaultRouter:{_ROUTERS[router]}:g" {self.project_name}/urls.py''')
 
-    def _set_auth_classes(self, project_name, auth_classes=None):
-        _AUTH = {
+    def _set_auth_classes(self, auth_classes=None):
+        """Sets Authentication classes to use in settings.py"""
+        A = {
             'key': 'DEFAULT_AUTHENTICATION_CLASSES',
             'token': 'rest_framework.authentication.TokenAuthentication',
             'session': 'rest_framework.authentication.SessionAuthentication',
@@ -131,30 +160,23 @@ class SetupDrfSeed:
         }
         if auth_classes:
             try:
-                REST_FRAMEWORK = __import__(
-                    f'{project_name}.settings',
-                    globals(),
-                    locals(),
-                     ['REST_FRAMEWORK']
-                ).REST_FRAMEWORK
-                if _AUTH['key'] in REST_FRAMEWORK.keys():
-                    print(f"{_AUTH['key']} exists, and contains: ")
-                    for v in REST_FRAMEWORK[_AUTH['key']]:
+                DRF = self.drf_settings
+                if A['key'] in DRF.keys():
+                    print(f"{A['key']} exists, and contains: ")
+                    for v in DRF[A['key']]:
                         print('\t' + v)
-                    print('\tSkipping, override manually')
+                    print('\t Skipping, override manually')
                 else:
-                    raise KeyError(f"{_AUTH['key']} not found, creating...")
-            except ModuleNotFoundError:
-                sys.exit(f'Could not locate {project_name}/settings.py, was '
-                        'this script run from the root dir?')
+                    raise KeyError(f"{A['key']} not found, creating...")
             except KeyError:
-                run(f"sed -i '/REST_FRAMEWORK/a\    ),' {project_name}/settings.py")
+                run(f"sed -i '/REST_FRAMEWORK/a\    ),' {self.settings_str}")
                 for a in auth_classes:
-                    run(f"""sed -i "/REST_FRAMEWORK/a\        '{_AUTH[a]}'," {project_name}/settings.py""")
-                run(f"""sed -i "/REST_FRAMEWORK/a\    '{_AUTH['key']}': (" {project_name}/settings.py""")
+                    run(f"""sed -i "/REST_FRAMEWORK/a\        '{A[a]}'," {self.settings_str}""")
+                run(f"""sed -i "/REST_FRAMEWORK/a\    '{A['key']}': (" {self.settings_str}""")
 
-    def _set_pagination(self, project_name, pag=None, size=20):
-        PAG = {
+    def _set_pagination(self, pag=None, size=20):
+        """Sets pagination class in settings.py"""
+        P = {
             'key': 'DEFAULT_PAGINATION_CLASS',
             'size_key': 'PAGE_SIZE',
             'page': 'rest_framework.pagination.PageNumberPagination',
@@ -163,77 +185,140 @@ class SetupDrfSeed:
         }
         if pag:
             if pag == 'none':
-                run(f'''sed -i "/{PAG['key']}/d" {project_name}/settings.py''')
-                run(f'''sed -i "/{PAG['size_key']}/d" {project_name}/settings.py''')
+                run(f'''sed -i "/{P['key']}/d" {self.settings_str}''')
+                run(f'''sed -i "/{P['size_key']}/d" {self.settings_str}''')
             else:
                 try:
-                    REST_FRAMEWORK = __import__(
-                        f'{project_name}.settings',
-                        globals(),
-                        locals(),
-                        ['REST_FRAMEWORK']
-                    ).REST_FRAMEWORK
-                    if PAG['key'] in REST_FRAMEWORK.keys():
-                        pag_val = REST_FRAMEWORK[PAG['key']]
-                        size_val = REST_FRAMEWORK[PAG['size_key']]
-                        print(f"{PAG['key']} exists, its value is: {pag_val}, with size {size_val} ")
-                        print(f'Replacing {pag_val} with {PAG[pag]}')
-                        print(f"Replacing {PAG['size_key']}: {size_val} with {PAG['size_key']}: {size}")
-                        run(f'''sed -i "s/{pag_val}/{PAG[pag]}/g" {project_name}/settings.py''')
-                        run(f'''sed -i "s/'{PAG['size_key']}': {size_val},/'{PAG['size_key']}': {size},/g" {project_name}/settings.py''')
+                    DRF = self.drf_settings
+                    if P['key'] in DRF.keys():
+                        pag_val = DRF[P['key']]
+                        size_val = DRF[P['size_key']]
+                        print(f"{P['key']} exists, its value is: {pag_val}, with size {size_val} ")
+                        print(f'Replacing {pag_val} with {P[pag]}')
+                        print(f"Replacing {P['size_key']}: {size_val} with {P['size_key']}: {size}")
+                        run(f'''sed -i "s/{pag_val}/{P[pag]}/g" {self.settings_str}''')
+                        run(f'''sed -i "s/'{P['size_key']}': {size_val},/'{P['size_key']}': {size},/g" {self.settings_str}''')
                     else:
-                        raise KeyError(f"{PAG['key']} not found, creating...")
-                except ModuleNotFoundError:
-                    sys.exit(f'Could not locate {project_name}/settings.py, was '
-                            'this script run from the root dir?')
+                        raise KeyError(f"{P['key']} not found, creating...")
                 except KeyError:
-                    run(f"""sed -i "/REST_FRAMEWORK/a\    '{PAG['key']}': '{PAG[pag]}'," {project_name}/settings.py""")
-                    run(f'''sed -i "/{PAG['key']}/a\    '{PAG['size_key']}': {size}," {project_name}/settings.py''')
-    
-    def _set_throttling(self, project_name, throttle_classes=None):
-        _THROTTLE = {
+                    run(f"""sed -i "/REST_FRAMEWORK/a\    '{P['key']}': '{P[pag]}'," {self.settings_str}""")
+                    run(f'''sed -i "/{P['key']}/a\    '{P['size_key']}': {size}," {self.settings_str}''')
+
+
+    def _set_throttling(self, classes=None):
+        """Sets throttling classes and rates in settings.py"""
+        T = {
             'classes': 'DEFAULT_THROTTLE_CLASSES',
             'rates': 'DEFAULT_THROTTLE_RATES',
             'anon': 'rest_framework.throttling.AnonRateThrottle',
             'user': 'rest_framework.throttling.UserRateThrottle',
+            'scoped': 'rest_framework.throttling.ScopedRateThrottle',
         }
-        # build scopes dict = {'anon': '1000/day', 'user': '1000/day', ...}
-        scopes = {s.split('=')[0]: s.split('=')[1] for s in throttle_classes}
-        # set default classes:
-        if throttle_classes:
+        if classes:
+            # build scopes dict = {'anon': '1000/day', 'user': '1000/day', ...}
+            scopes = {
+                s.split('=')[0]: s.split('=')[1].replace('/','/') if '=' in s else s for s in classes
+            }
+            scopes['authtoken'] = '10/minute'
             try:
-                REST_FRAMEWORK = __import__(
-                    f'{project_name}.settings',
-                    globals(),
-                    locals(),
-                     ['REST_FRAMEWORK']
-                ).REST_FRAMEWORK
-                if _THROTTLE['rates'] in REST_FRAMEWORK.keys():
-                    print(f"{_THROTTLE['rates']} exists, and contains: ")
-                    for k, v in REST_FRAMEWORK[_THROTTLE['rates']].items():
-                        print('\t' + k + ': ' + v)
-                    
+                DRF = self.drf_settings
+                if T['classes'] in DRF.keys():
+                    print(f"{T['classes']} exists, contains:")
+                    for c in DRF[T['classes']]:
+                        print(f"\t {c}")
+                    for s in scopes.keys():
+                        cond1 = s in ('anon', 'user', 'scoped')
+                        if cond1:
+                            cond2 = T[s] not in DRF[T['classes']]
+                        if cond2:
+                            run(f'''sed -i "{T['classes']}/a\        {T[s]}," {self.settings_str}''')
                 else:
-                    raise KeyError(f"{_THROTTLE['key']} not found, creating...")
-            except ModuleNotFoundError:
-                sys.exit(f'Could not locate {project_name}/settings.py, was '
-                        'this script run from the root dir?')
+                    raise KeyError(f"{T['classes']} not found, creating...")
             except KeyError:
-                pass
-                # run(f"sed -i '/REST_FRAMEWORK/a\    ),' {project_name}/settings.py")
-                # for a in auth_classes:
-                #     run(f"""sed -i "/REST_FRAMEWORK/a\        '{_AUTH[a]}'," {project_name}/settings.py""")
-                # run(f"""sed -i "/REST_FRAMEWORK/a\    '{_AUTH['key']}': (" {project_name}/settings.py""")
+                run(f"sed -i '/REST_FRAMEWORK/a\    ),' {self.settings_str}")
+                for s in scopes.keys():
+                    if s in ('anon', 'user', 'scoped'):
+                        run(f"""sed -i "/REST_FRAMEWORK/a\        '{T[s]}'," {self.settings_str}""")
+                run(f"""sed -i "/REST_FRAMEWORK/a\    '{T['classes']}': (" {self.settings_str}""")
+            try:
+                if T['rates'] in DRF.keys():
+                    print(f"{T['rates']} exists, contains:")
+                    for r in DRF[T['rates']]:
+                        print(f"\t{r}")
+                        for k, v in scopes.items():
+                            cond1 = k != 'scoped'
+                            cond2 = k not in DRF[T['rates']].keys()
+                            if cond1 and cond2:
+                                run(f"""sed -i "/{T["rates"]}/a\        '{k}': '{v}'," {self.settings_str}""")
+                else:
+                    raise KeyError(f"{T['rates']} not found, creating...")
+            except KeyError:
+                run(f"sed -i '/REST_FRAMEWORK/a\    }},' {self.settings_str}")
+                for k, v in scopes.items():
+                    if k != 'scoped':
+                        run(f"""sed -i "/REST_FRAMEWORK/a\        '{k}': '{v}'," {self.settings_str}""")
+                run(f"""sed -i "/REST_FRAMEWORK/a\    '{T['rates']}': {{" {self.settings_str}""")
     
+    def _set_static_root(self, dir_name):
+        hook = 'REST_FRAMEWORK'
+        if dir_name:
+            if hasattr(self.settings, 'STATIC_ROOT'):
+                print(f'STATIC_ROOT exists, replacing value to {dir_name}')
+                run(f'''sed -i "/STATIC_ROOT/d" {self.settings_str}''')
+                run(f'''sed -i "/{hook}/i\STATIC_ROOT = os.path.join(BASE_DIR, '{dir_name}')" {self.settings_str}''')
+            else:
+                print(f'STATIC_ROOT not found, creating...')
+                run(f'''sed -i "/{hook}/i\STATIC_ROOT = os.path.join(BASE_DIR, '{dir_name}')" {self.settings_str}''')
 
+    def _set_static_url(self, url_name):
+        hook = 'REST_FRAMEWORK'
+        if url_name:
+            if hasattr(self.settings, 'STATIC_URL'):
+                print(f'STATIC_URL exists, replacing value to {url_name}')
+                run(f'''sed -i "/STATIC_URL/d" {self.settings_str}''')
+                run(f'''sed -i "/{hook}/i\STATIC_URL= '/{url_name}/'" {self.settings_str}''')
+            else:
+                print(f'STATIC_URL not found, creating...')
+                run(f'''sed -i "/{hook}/i\STATIC_URL = '/{url_name}/'" {self.settings_str}''')
 
+    def _set_media_root(self, dir_name):
+        hook = 'REST_FRAMEWORK'
+        if dir_name:
+            if hasattr(self.settings, 'MEDIA_ROOT'):
+                print(f'MEDIA_ROOT exists, replacing value to {dir_name}')
+                run(f'''sed -i "/MEDIA_ROOT/d" {self.settings_str}''')
+                run(f'''sed -i "/{hook}/i\MEDIA_ROOT = os.path.join(BASE_DIR, '{dir_name}')" {self.settings_str}''')
+            else:
+                print(f'MEDIA_ROOT not found, creating...')
+                run(f'''sed -i "/{hook}/i\MEDIA_ROOT = os.path.join(BASE_DIR, '{dir_name}')" {self.settings_str}''')
+
+    def _set_media_url(self, url_name):
+        hook = 'REST_FRAMEWORK'
+        if url_name:
+            if hasattr(self.settings, 'MEDIA_URL'):
+                print(f'MEDIA_URL exists, replacing value to {url_name}')
+                run(f'''sed -i "/MEDIA_URL/d" {self.settings_str}''')
+                run(f'''sed -i "/{hook}/i\MEDIA_URL= '/{url_name}/'" {self.settings_str}''')
+            else:
+                print(f'MEDIA_URL not found, creating...')
+                run(f'''sed -i "/{hook}/i\MEDIA_URL = '/{url_name}/'" {self.settings_str}''')
+            run(f'''sed -i "/import settings/d" {self.project_name}/urls.py''')
+            run(f'''sed -i "/import static/d" {self.project_name}/urls.py''')
+            run(f'''sed -i "/from django.conf.urls/i\\from django.conf import settings" {self.project_name}/urls.py''')
+            run(f'''sed -i "/from django.conf.urls/a\\from django.conf.urls.static import static" {self.project_name}/urls.py''')
+            run(f'''sed -i "/static(settings.MEDIA_URL,/d" {self.project_name}/urls.py''')
+            run(r'''sed -i "$a\urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)" {name}/urls.py'''.format(name=self.project_name))
 
 if __name__ == '__main__':
-    shell = SetupDrfSeed()
-    # shell._create_virtualenv()
-    # shell._install_dependencies(shell._args.proxy)
-    print(shell._args)
-    shell._set_router(shell._args.project_name, shell._args.router)
-    shell._set_auth_classes(shell._args.project_name, shell._args.auth)
-    shell._set_pagination(shell._args.project_name, shell._args.pagination, shell._args.page_size)
-    shell._set_throttling(shell._args.project_name, shell._args.throttling)
+    s = SetupDrfSeed()
+    # s._create_virtualenv()
+    # s._install_dependencies(s._args.proxy)
+    print(s._args)
+    s._set_router(s._args.router)
+    s._set_auth_classes(s._args.auth)
+    s._set_pagination(s._args.pagination, s._args.page_size)
+    s._set_throttling(s._args.throttling)
+    s._set_static_root(s._args.static_root)
+    s._set_static_url(s._args.static_url)
+    s._set_media_root(s._args.media_root)
+    s._set_media_url(s._args.media_url)
