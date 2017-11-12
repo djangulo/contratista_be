6b30f19719fd16e8ad915a539060ed89721106fe
djangulo <denis.angulo@linekode.com>
2017-11-12 19:20:18 -0400

    Changed DB settings to postgresql and added the psycopg2 package to requirements.txt

--- contratista_be/settings.py
+++ contratista_be/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'contratista_be.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'contratista_cluster',
+        'USER': 'contratista_user',
+        'PASSWORD': 'Tropicana127',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
