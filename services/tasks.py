f9e1ee752016d94eb07582e11cdd8c6d17a9a39c
Denis Angulo <denis.angulo@linekode.com>
2017-11-20 15:04:19 -0500

    dummy tasks.py in services app

---
+++ services/tasks.py
@@ -0,0 +1,7 @@
+# from .models import Address
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+
+# @receiver(post_save, sender=Address)
+# def get_address_latlong(sender, instance, **kwargs):
+#     pass
